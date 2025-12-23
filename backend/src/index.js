1a1060bfa63de676ac9d30f98970cf6ffd2d8be6
Arya Pratik <aryapratikofficial@gmail.com>
2025-12-23 18:49:34 +0530

    backend: initial setup with express and mongodb

---
+++ backend/src/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("Backend running");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
