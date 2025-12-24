02560653e48c96d4b4d5e89f29e4c3cd9a7717ec
Arya Pratik <aryapratikofficial@gmail.com>
2025-12-24 20:22:05 +0530

    auth: google token verification and jwt issue

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-
+import authRoutes from "./routes/auth.routes.js";
 dotenv.config();
 
 const app = express();
@@ -14,6 +14,7 @@ connectDB();
 app.get("/", (req, res) => {
   res.send("Backend running");
 });
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
