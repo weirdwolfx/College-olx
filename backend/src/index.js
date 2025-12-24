e3ab4effbf5c3356ae8ef4e1b47fea04d5fad041
Arya Pratik <aryapratikofficial@gmail.com>
2025-12-24 21:47:39 +0530

    feat: core listings APIs (create, get all, get my listings)

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/auth.routes.js";
 import testRoutes from "./routes/test.routes.js"; 
+import listingRoutes from "./routes/listing.routes.js";
 
 dotenv.config();
 
@@ -18,6 +19,9 @@ app.get("/", (req, res) => {
 });
 app.use("/api/auth", authRoutes);
 app.use("/api/test", testRoutes); 
+app.use("/api/listings", listingRoutes);
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
