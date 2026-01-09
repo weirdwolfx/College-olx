90589dc8379ea6c966791392334e2e0d466b6696
devendra000001 <devendrakhatke20@gmail.com>
2026-01-09 14:11:02 +0530

    Update index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { BrowserRouter } from "react-router-dom";
 
+import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 import AuthWrapper from "./components/AuthWrapper";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
- <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+  <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
     <AuthProvider>
-      <AuthWrapper />
+      <BrowserRouter>
+        <AuthWrapper>
+          <App />
+        </AuthWrapper>
+      </BrowserRouter>
     </AuthProvider>
   </GoogleOAuthProvider>
 );
