64b97d658ab8b75705c2642d216036049a18a808
devendra000001 <devendrakhatke20@gmail.com>
2026-01-09 14:15:46 +0530

    Integrate BrowserRouter and update GoogleOAuthProvider

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { BrowserRouter } from "react-router-dom";
 
+import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 import AuthWrapper from "./components/AuthWrapper";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
- <GoogleOAuthProvider clientId="679393911042-f7fs9d5f325t7ce8qfpr3fn46g0c5nfq.apps.googleusercontent.com" >
+  <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
     <AuthProvider>
-      <AuthWrapper />
+      <BrowserRouter>
+        <AuthWrapper>
+          <App />
+        </AuthWrapper>
+      </BrowserRouter>
     </AuthProvider>
   </GoogleOAuthProvider>
 );
